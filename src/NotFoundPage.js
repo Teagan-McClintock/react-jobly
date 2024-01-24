8e7d60026f5d12af135efd4c8cb28b579ca3c9a1
jswwWorks <jsww.works@gmail.com>
2024-01-24 17:36:18 -0500

    Created NotFoundPage for when user navigates to bad url

---
+++ src/NotFoundPage.js
@@ -0,0 +1,18 @@
+
+/**
+ *  NotFoundPage renders when a user inputs an invalid URL.
+ *
+ *  Props: none
+ *
+ *  States: none
+ *
+ *  Renders:
+ *    RoutesList -> NotFoundPage
+ */
+function NotFoundPage() {
+  return (
+    <p>Sorry, the page you're looking for doesn't exist.</p>
+  );
+}
+
+export default NotFoundPage;
