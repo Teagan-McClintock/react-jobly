44c1e9679916eebf45769e990e25dddd7767f6c4
jswwWorks <jsww.works@gmail.com>
2024-01-24 19:32:21 -0500

    Added styling to Homepage font, added more detailing to Navigation element

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,4 +1,5 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import "./Navigation.css";
 
 /**Navigation stays on the top of the screen and contains links to
  *  homepage, JobList, and CompanyList
@@ -15,18 +16,27 @@ function Navigation() {
   const activeState = ({ isActive, isPending }) => {
     return {
     color: isPending ? "rgb(253 230 138)" : "",
-    backgroundColor: isActive ? "rgb(69 26 3)" : "",
-    fontWeight: isActive ? "bold" : ""
+    backgroundColor: isActive ? "rgb(213, 213, 241)" : ""
     };
   };
 
   return (
     <header className="Navigation">
-      <NavLink to={"/"} style={activeState}>Jobly</NavLink>
-      <NavLink to={"/companies"} style={activeState}>Companies</NavLink>
-      <NavLink to={"/jobs"} style={activeState}>Jobs</NavLink>
+      <div className="Navigation-links">
+        <NavLink to={"/"} style={activeState}>Jobly</NavLink>
+        <NavLink to={"/companies"} style={activeState}>Companies</NavLink>
+        <NavLink to={"/jobs"} style={activeState}>Jobs</NavLink>
+      </div>
     </header>
   ) // use NavLink instead of Link here (come back to this when we style)
 }
 
 export default Navigation;
+
+// <div className="Navigation">
+// <header className="Navigation">
+//   <NavLink to={"/"} style={activeState}>Jobly</NavLink>
+//   <NavLink to={"/companies"} style={activeState}>Companies</NavLink>
+//   <NavLink to={"/jobs"} style={activeState}>Jobs</NavLink>
+// </header>
+// </div>
